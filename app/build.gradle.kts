343f1fd1078dfa1408cf8451206e6461b1bc3490
SuperCdat <126152622+SuperCdat@users.noreply.github.com>
2025-04-22 18:49:52 +0000

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "io.github.supercdat.storesuper"
         minSdk = 21
         targetSdk = 34
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
